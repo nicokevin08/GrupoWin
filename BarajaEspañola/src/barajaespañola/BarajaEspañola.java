2c1df0bcd06ce5985f4e94caae666b5c61fc8238
Cadelago Nicolas <nicokevin08@hotmail.com>
2024-03-21 19:34:45 -0300

    Primeros cambios

--- BarajaEspañola/src/barajaespañola/BarajaEspañola.java
+++ BarajaEspañola/src/barajaespañola/BarajaEspañola.java
@@ -1,10 +1,9 @@
-
 package barajaespañola;
 
 public class BarajaEspañola {
 
     public static void main(String[] args) {
-    Baraja baraja = new Baraja();
+        Baraja baraja = new Baraja();
         baraja.barajar();
         System.out.println("Cartas disponibles en la baraja: " + baraja.cartasDisponibles());
         System.out.println("Mostrando algunas cartas:");
@@ -18,21 +17,3 @@ public class BarajaEspañola {
         baraja.mostrarBaraja();
     }
 }
-
-class Carta {
-    private int numero;
-    private String palo;
-
-    public Carta(int numero, String palo) {
-        this.numero = numero;
-        this.palo = palo;
-    }
-
-    @Override
-    public String toString() {
-        return numero + " de " + palo;
-    }
-}
-    
-    
-
---
+++ BarajaEspañola/src/barajaespañola/Carta.java
@@ -0,0 +1,18 @@
+package barajaespañola;
+
+public class Carta {
+
+    private int numero;
+    private String palo;
+
+    public Carta(int numero, String palo) {
+        this.numero = numero;
+        this.palo = palo;
+    }
+
+    @Override
+    public String toString() {
+        return numero + " de " + palo;
+    }
+
+}
